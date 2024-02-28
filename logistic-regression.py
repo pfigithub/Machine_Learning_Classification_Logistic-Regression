6afdb2a070b1320f7a7b89815caf6d426934d165
pfigithub <pampam7072@gmail.com>
2024-02-28 15:40:46 +0330

    modeling and predicting

--- logistic-regression.py
+++ logistic-regression.py
@@ -26,3 +26,11 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
 print ('Train set:', X_train.shape,  y_train.shape)
 print ('Test set:', X_test.shape,  y_test.shape)
+
+# modeling
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+LR = LogisticRegression(C=0.01, solver='liblinear').fit(X_train,y_train)
+# predicing
+yhat = LR.predict(X_test)
+yhat_prob = LR.predict_proba(X_test)
